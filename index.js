acb2f551160b2985972a2956828992769b37da54
Lauren Perchitti <laurenperchitti@gmail.com>
2018-06-25 21:40:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ function scuberGreetingForFeet(someValue) {
 
   let result
   if (someValue <= 400) {
-    result = "This one is on me!"
+    result = "This one is on me!";
   }
     else if (someValue > 2000) {
-      result = "I will gladly take your thirty bucks."
+      result = "I will gladly take your thirty bucks.";
     }
-    else (someValue > 2500) {
+    else if (someValue > 2500) {
       result = 'No can do'
     }
   }
